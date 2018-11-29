7ff0539c42cd16a7db37b43c9c4516b35e101b87
Quentin Quaadgras <email@quentinquaadgras.com>
2018-11-29 14:33:16 +1300

    Add better GoFromJs support.

--- script.go
+++ script.go
@@ -3,6 +3,7 @@ package seed
 import (
 	"fmt"
 	"reflect"
+	"strings"
 	"net/http"
 )
 
@@ -76,13 +77,35 @@ type ExportedFunction struct {
 
 var exports = make(map[string]reflect.Value)
 
-func (q Script) Run(f interface{}) {
-	if name, ok := f.(string); ok {
+func (q Script) Run(f interface{}, args ...qlova.Type) {
+	if name, ok := f.(string); ok && len(args) == 0 {
 		q.Raw("Javascript", language.Statement(name+`();`))
 		return 
 	}
 	
-	panic("script.Run(func()): Unimplemented")
+	var name = fmt.Sprint(f)
+	
+	var value = reflect.ValueOf(f)
+	
+	if value.Kind() != reflect.Func || value.Type().NumOut() != 0 {
+		panic("Script.Call: Must pass a Go function without return values")
+	}
+	exports[name] = value
+	
+	var CallingString = `/call/`+name
+	
+	for i := 0; i < value.Type().NumIn(); i++ {
+		switch value.Type().In(i).Kind() {
+			case reflect.String:
+				
+				CallingString += `/_"+encodeURIComponent(`+args[i].(qlova.String).Raw()+`)+"`
+				
+			default:
+				panic("Unimplemented: script.Run("+value.Type().String()+")")
+		}
+	}
+	
+	q.Raw("Javascript", language.Statement(`let request = new XMLHttpRequest(); request.open("POST", "`+CallingString+`"); request.send();`))
 }
 
 //Export a Go function to Javascript. Don't use this for non-local apps! TODO enforce this
@@ -119,12 +142,38 @@ func (q Script) Call(f interface{}) qlova.Type {
 }
 
 func callHandler(w http.ResponseWriter, r *http.Request, call string) {
-	f, ok := exports[call]
+	var args = strings.Split(call, "/")
+	if len(args) == 0 {
+		return
+	}
+	
+	f, ok := exports[args[0]]
 	if !ok {
 		return
 	}
 	
-	var results = f.Call(nil)
+	if len(args)-1 != f.Type().NumIn() {
+		println("argument length mismatch")
+		return
+	}
+	
+	var in []reflect.Value
+	for i := 0; i < f.Type().NumIn(); i++ {
+		switch f.Type().In(i).Kind() {
+			case reflect.String:
+				
+				in = append(in, reflect.ValueOf(args[i+1][1:]))
+				
+			default:
+				println("unimplemented callHandler for "+f.Type().String())
+				return
+		}
+	}
+	
+	var results = f.Call(in)
+	if len(results) == 0 {
+		return
+	}
 	switch results[0].Kind() {
 		
 		case reflect.String:
