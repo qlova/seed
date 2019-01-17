d0485c52f901e11dfd87a364c1574f4c94078e0c
Quentin Quaadgras <quentin@splizard.com>
2019-01-18 12:57:04 +1300

    Redundent.

--- application.go
+++ application.go
@@ -3,9 +3,6 @@ package seed
 
 type Application struct {
 	Seed
-
-	//These booleans enable the application to serve different content depending on the user agent passed to the server.
-	Desktop, Mobile, Tablet, Watch bool
 }
 
 func NewApp() Application {
