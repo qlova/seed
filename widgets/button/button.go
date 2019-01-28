542d933a18b7358c936a1987101f6ca89cfd3a87
Quentin Quaadgras <email@quentinquaadgras.com>
2019-01-28 23:52:48 +1300

    Allow button widget to take parameters.

--- widgets/button/button.go
+++ widgets/button/button.go
@@ -6,18 +6,22 @@ type Widget struct {
 	seed.Seed
 }
 
-func New() Widget {
+func New(text ...string) Widget {
 	widget := seed.New()
 	
 	widget.SetTag("button")
 	
 	widget.SetSize(seed.Auto, seed.Auto)
+	
+	if len(text) > 0 {
+		widget.SetText(text[0])
+	}
 
 	return Widget{widget}
 }
 
-func AddTo(parent seed.Interface) Widget {
-	var widget = New()
+func AddTo(parent seed.Interface, text ...string) Widget {
+	var widget = New(text...)
 	parent.Root().Add(widget)
 	return widget
 }
