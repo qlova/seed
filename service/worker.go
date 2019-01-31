04da2fe36022a5918efa896950acd1c81c17639c
Quentin Quaadgras <quentin@splizard.com>
2019-01-31 15:51:44 +1300

    Only fetch if we are online.

--- service/worker.go
+++ service/worker.go
@@ -42,11 +42,15 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.open('mysite-dynamic').then(function(cache) {
       return cache.match(event.request).then(function(response) {
-        var fetchPromise = fetch(event.request).then(function(networkResponse) {
-          cache.put(event.request, networkResponse.clone());
-          return networkResponse;
-        })
-        return response || fetchPromise;
+      	if (navigator.onLine) {
+	        var fetchPromise = fetch(event.request).then(function(networkResponse) {
+	          cache.put(event.request, networkResponse.clone());
+	          return networkResponse;
+	        })
+	        return response || fetchPromise;
+	    } else {
+	    	return response;
+	    }
       })
     })
   );
