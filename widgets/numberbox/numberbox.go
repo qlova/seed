5f76fc3f7988fb6a4c09d9886e1f7126e59482c9
Quentin Quaadgras <quentin@splizard.com>
2019-02-20 10:43:19 +1300

    Add numebrbox.

---
+++ widgets/numberbox/numberbox.go
@@ -0,0 +1,22 @@
+package numberbox
+
+import "github.com/qlova/seed"
+
+import "github.com/qlova/seed/widgets/textbox"
+
+type Widget struct {
+	textbox.Widget
+}
+
+func New() Widget {
+	var TextBox = textbox.New()
+		TextBox.SetAttributes("type='number'")
+	
+	return Widget{TextBox}
+}
+
+func AddTo(parent seed.Interface) Widget {
+	var widget = New()
+	parent.Root().Add(widget)
+	return widget
+}
