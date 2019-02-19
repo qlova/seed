0e5b070eff10035cca9e06b5c2be0d9c34dad925
Quentin Quaadgras <quentin@splizard.com>
2019-02-19 13:38:03 +1300

    Simple service worker.

--- service/worker.go
+++ service/worker.go
@@ -40,19 +40,9 @@ func (worker Worker) Render() []byte {
 	
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.open('mysite-dynamic').then(function(cache) {
-      return cache.match(event.request).then(function(response) {
-      	if (navigator.onLine) {
-	        var fetchPromise = fetch(event.request).then(function(networkResponse) {
-	          cache.put(event.request, networkResponse.clone());
-	          return networkResponse;
-	        })
-	        return response || fetchPromise;
-	    } else {
-	    	return response;
-	    }
-      })
-    })
+  	caches.match(event.request).then(function(response) {
+  		return response || fetch(event.request);
+  	})
   );
 });
 `)
