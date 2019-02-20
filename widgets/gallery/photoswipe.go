8d5636ff59002f1a238f40c8bd457e39bc6e5e6d
Quentin Quaadgras <quentin@splizard.com>
2019-02-20 15:58:30 +1300

    Automatically figure out image dimensions for the gallery.

--- widgets/gallery/photoswipe.go
+++ widgets/gallery/photoswipe.go
@@ -3,49 +3,45 @@ package gallery
 import "github.com/qlova/seed"
 import "github.com/qlova/seed/script"
 
+import (
+	_ "image/jpeg"
+    _ "image/png"
+    "os"
+    "fmt"
+    "image"
+    
+)
+
 func init() {
 	seed.Embed("/photoswipe.js", []byte(Javascript))
 	seed.Embed("/photoswipe.css", []byte(CSS))
 	seed.Embed("/photoswipe-ui.js", []byte(UI))
 
 	seed.Tail(`	
-	<!-- Root element of PhotoSwipe. Must have class pswp. -->
 	<div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
-
-    <!-- Background of PhotoSwipe. 
-         It's a separate element, as animating opacity is faster than rgba(). -->
     <div class="pswp__bg"></div>
 
-    <!-- Slides wrapper with overflow:hidden. -->
     <div class="pswp__scroll-wrap">
 
-        <!-- Container that holds slides. PhotoSwipe keeps only 3 slides in DOM to save memory. -->
+
         <div class="pswp__container">
-            <!-- don't modify these 3 pswp__item elements, data is added later on -->
             <div class="pswp__item"></div>
             <div class="pswp__item"></div>
             <div class="pswp__item"></div>
         </div>
 
-        <!-- Default (PhotoSwipeUI_Default) interface on top of sliding area. Can be changed. -->
+
         <div class="pswp__ui pswp__ui--hidden">
 
             <div class="pswp__top-bar">
 
-                <!--  Controls are self-explanatory. Order can be changed. -->
-
+                <!--  Controls are hidden. -->
                 <div style="display:none;" class="pswp__counter"></div>
-
                 <button style="display:none;" class="pswp__button pswp__button--close" title="Close (Esc)"></button>
-
                 <button style="display:none;" class="pswp__button pswp__button--share" title="Share"></button>
-
                 <button style="display:none;" class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button>
-
                 <button style="display:none;" class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>
 
-                <!-- Preloader demo https://codepen.io/dimsemenov/pen/yyBWoR -->
-                <!-- element will get class pswp__preloader--active when preloader is running -->
                 <div class="pswp__preloader">
                     <div class="pswp__preloader__icn">
                       <div class="pswp__preloader__cut">
@@ -80,7 +76,23 @@ type Widget struct {
 	seed.Seed
 }
 
-//Returns a full-featured text editor with line numbers and optional syntax highlighting.
+func getImageDimension(imagePath string) string {
+    file, err := os.Open(imagePath)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "%v\n", err)
+    }
+
+    image, _, err := image.DecodeConfig(file)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "%s: %v\n", imagePath, err)
+    }
+
+	file.Close()
+    
+    return fmt.Sprint("w:",image.Width,",h:", image.Height)
+}
+
+//Returns gallery that displays 'local' images (in the assets directory).
 func New(images ...string) Widget {
 	gallery := seed.New()
 
@@ -91,7 +103,10 @@ func New(images ...string) Widget {
 	gallery.OnReady(func(q seed.Script) {
 		q.Javascript(gallery.Script(q).Element()+".items = [")
 		for i, img := range images {
-			q.Javascript(`{src:"`+img+`", w:900, h:600 }`)
+
+			var dimensions = getImageDimension(seed.Dir+"/assets/"+img)
+		
+			q.Javascript(`{src:"`+img+`", `+dimensions+` }`)
 			if i < len(images)-1 {
 				q.Javascript(`,`)
 			}
@@ -102,7 +117,6 @@ func New(images ...string) Widget {
 	return Widget{gallery}
 }
 
-//Create a new Text widget and add it to the provided parent.
 func AddTo(parent seed.Interface, images ...string) Widget {
 	var Gallery = New(images...)
 	parent.Root().Add(Gallery)
