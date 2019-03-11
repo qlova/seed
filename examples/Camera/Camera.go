ab538f6ed627044ec090c2fb5d8184bd46d42cfc
Quentin Quaadgras <email@quentinquaadgras.com>
2019-03-11 21:20:40 +1300

    Add camera widget and example app.

---
+++ examples/Camera/Camera.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/qlova/seed"
+import "github.com/qlova/seed/widgets/camera"
+
+func main() {
+	var App = seed.NewApp("Camera")
+		App.SetSize(100, 100)
+		App.SetColor(seed.RGB(0,0,0))
+
+	var Camera = camera.AddTo(App)
+		Camera.SetSize(100, 100)
+	
+	App.Launch()
+}
