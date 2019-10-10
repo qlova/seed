cc5dc648c00c074f043dbef47cfb220f4e09b78a
Quentin Quaadgras <em12256@wb410-765wsm.autuni.aut.ac.nz>
2019-10-10 17:39:23 +1300

    RemoveShadow style methods

--- style/shadow.go
+++ style/shadow.go
@@ -19,7 +19,17 @@ func (style Style) SetShadow(shadow Shadow) {
 	style.Set("box-shadow", fmt.Sprint(css.Decode(shadow.X), " ", css.Decode(shadow.Y), " ", css.Decode(shadow.Blur), " ", css.Decode(shadow.Spread), " ", css.Colour(shadow.Color)))
 }
 
+//RemoveShadow removes the shadow on this element.
+func (style Style) RemoveShadow() {
+	style.Set("box-shadow", "unset")
+}
+
 //SetTextShadow sets the elements text style to match the Shadow defintion.
 func (style Style) SetTextShadow(shadow Shadow) {
 	style.Set("text-shadow", fmt.Sprint(css.Decode(shadow.X), " ", css.Decode(shadow.Y), " ", css.Decode(shadow.Blur), " ", css.Colour(shadow.Color)))
+}
+
+//RemoveTextShadow removes the text shadow on this element.
+func (style Style) RemoveTextShadow() {
+	style.Set("text-shadow", "")
 }
