e3a21965db6f31df4c6d8e5e26bcd9cbab6a6dd2
Quentin Quaadgras <em12256@wb410-765wsm.autuni.aut.ac.nz>
2019-11-12 15:39:41 +1300

    Add dynamic.Array conversion.

--- script/dynamic.go
+++ script/dynamic.go
@@ -12,3 +12,9 @@ func (d Dynamic) String() String {
 	return d.Q.Value(fmt.Sprintf(`(typeof %v == "string" && %v) || ""`,
 		d.LanguageType().Raw(), d.LanguageType().Raw())).String()
 }
+
+//Array returns the dynamic value as an array.
+func (d Dynamic) Array() Array {
+	return d.Q.Value(fmt.Sprintf(`(typeof %v == "array" && %v) || []`,
+		d.LanguageType().Raw(), d.LanguageType().Raw())).Array()
+}
