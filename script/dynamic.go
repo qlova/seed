9e107cad0fbefa6c863d489d0c5151782f882211
Quentin Quaadgras <quentin@splizard.com>
2019-11-22 11:03:18 +1300

    Add brackets around dynamic conversions.

--- script/dynamic.go
+++ script/dynamic.go
@@ -9,18 +9,18 @@ type Dynamic struct {
 }
 
 func (d Dynamic) String() String {
-	return d.Q.Value(fmt.Sprintf(`%v || ""`,
+	return d.Q.Value(fmt.Sprintf(`(%v||"")`,
 		d.LanguageType().Raw())).String()
 }
 
 //Object returns the dynamic value as an object.
 func (d Dynamic) Object() Object {
-	return d.Q.Value(fmt.Sprintf(`%v || {}`,
+	return d.Q.Value(fmt.Sprintf(`(%v||{})`,
 		d.LanguageType().Raw())).Object()
 }
 
 //Array returns the dynamic value as an array.
 func (d Dynamic) Array() Array {
-	return d.Q.Value(fmt.Sprintf(`%v || []`,
+	return d.Q.Value(fmt.Sprintf(`(%v||[])`,
 		d.LanguageType().Raw())).Array()
 }
