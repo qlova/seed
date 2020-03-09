4507919a8b5f5574d89b4e933af05b67c3418ee7
Splizard <quentin@splizard.com>
2020-03-09 16:07:56 +1300

    Add some exported css types.

--- css/functions.go
+++ css/functions.go
@@ -18,3 +18,18 @@ func (AnimationName) animationNameValue() {}
 func (a AnimationName) Rule() Rule {
 	return Rule(a)
 }
+
+type Number float64
+
+func (Number) numberValue() {}
+func (n Number) Rule() Rule {
+	return Rule(fmt.Sprintf("%f", n))
+}
+
+type Int int
+
+func (Int) integerOrAutoValue()           {}
+func (Int) animationIterationCountValue() {}
+func (i Int) Rule() Rule {
+	return Rule(fmt.Sprintf("%v", i))
+}
