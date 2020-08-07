0136b0d13f44038fcf0d9c4f985f21816ca5fcde
Quentin Quaadgras <email@quentinquaadgras.com>
2020-08-07 15:34:31 +1200

    Add column.Set and column.Wrap

--- s/column/column.go
+++ s/column/column.go
@@ -19,3 +19,13 @@ func New(options ...seed.Option) seed.Seed {
 		seed.Options(options),
 	)
 }
+
+//Set returns an option that sets the seed to layout children in a column.
+func Set() css.Rule {
+	return css.SetFlexDirection(css.Column)
+}
+
+//Wrap returns an option that sets the seed to wrap it's children into multiple columns.
+func Wrap() css.Rule {
+	return css.SetFlexWrap(css.Wrap)
+}
