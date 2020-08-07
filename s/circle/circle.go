11e4d452c302d66dba680e8875b14fbe6215f72a
Quentin Quaadgras <email@quentinquaadgras.com>
2020-08-07 15:34:20 +1200

    Adjust circle package to use the new units.Unit

--- s/circle/circle.go
+++ s/circle/circle.go
@@ -3,15 +3,23 @@ package circle
 import (
 	"qlova.org/seed"
 	"qlova.org/seed/s/column"
-	"qlova.org/seed/style"
+	"qlova.org/seed/set"
+	"qlova.org/seed/units"
+	"qlova.org/seed/units/percentage/of"
 )
 
 //New returns a new circle.
-func New(radius style.Unit, options ...seed.Option) seed.Seed {
+func New(options ...seed.Option) seed.Seed {
 	return column.New(
-		style.SetSize(radius, radius),
-		style.SetRoundedCorners(50),
 
 		seed.Options(options),
 	)
+}
+
+//Set sets the radius of the circle.
+func Set(radius units.Unit) seed.Option {
+	return seed.Options{
+		set.Size(radius, radius),
+		set.Rounded(50 % of.Parent),
+	}
 }
