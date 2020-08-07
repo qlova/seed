6c8ac42ad7e20f67e8661f9f2141480a30f2e2e3
Quentin Quaadgras <email@quentinquaadgras.com>
2020-08-07 15:34:40 +1200

    Add filepicker.Update

--- s/filepicker/filepicker.go
+++ s/filepicker/filepicker.go
@@ -19,6 +19,20 @@ func New(options ...seed.Option) seed.Seed {
 	return input.New(attr.Set("type", "file"), seed.Options(options))
 }
 
+//Update the attachment when a file is picked.
+func Update(a attachment.Attachment) seed.Option {
+	return seed.NewOption(func(c seed.Seed) {
+		c.With(
+			client.OnInput(js.Script(func(q script.Ctx) {
+				q(js.Global().Get("seed").Set("active", script.Element(c)))
+				q("try {")
+				q(a.Set(js.NewValue(`event.target.files[0]`)))
+				q("} catch(e) { seed.report(e) }")
+			})),
+		)
+	})
+}
+
 //SelectFile asks the user to select a file, if and only if they do select a file, the handler will be called.
 //There is no way to tell if the user did not select a file.
 func SelectFile(handler func(File) client.Script) script.Script {
