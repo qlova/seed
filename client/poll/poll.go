937a2051abcc445a10996b89c400db819d950207
Quentin Quaadgras <email@quentinquaadgras.com>
2021-02-16 15:08:30 +1300

    Allow any function to be polled.

--- client/poll/poll.go
+++ client/poll/poll.go
@@ -12,13 +12,13 @@ import (
 )
 
 //When the condition is true, poll is called at the given interval.
-func When(condition client.Bool, interval time.Duration, poll interface{}) seed.Option {
-	return client.OnLoad(window.SetInterval(client.If(condition, client.Go(poll)).GetScript(), js.NewNumber(float64(interval.Milliseconds()))))
+func When(condition client.Bool, interval time.Duration, poll client.Script) seed.Option {
+	return client.OnLoad(window.SetInterval(client.If(condition, poll).GetScript(), js.NewNumber(float64(interval.Milliseconds()))))
 }
 
-//Every calls pol at every interval as long as the seed is visible.
-//If you want pol to be called regardless if the seed is visible, use When(client.True, interval, poll)
-func Every(interval time.Duration, poll interface{}) seed.Option {
+//Every calls poll at every interval as long as the seed is visible.
+//If you want poll to be called regardless if the seed is visible, use When(client.True, interval, poll)
+func Every(interval time.Duration, poll client.Script) seed.Option {
 	return seed.NewOption(func(c seed.Seed) {
 		When(html.Element(c).Get("offsetParent"), interval, poll).AddTo(c)
 	})
