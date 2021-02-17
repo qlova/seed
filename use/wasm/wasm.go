d8e0613a781ef23f746999cc735531299b8c20c7
Quentin Quaadgras <email@quentinquaadgras.com>
2021-02-17 21:59:11 +1300

    Add wasm io.Reader support when recieving files.

--- use/wasm/wasm.go
+++ use/wasm/wasm.go
@@ -4,7 +4,9 @@ package wasm
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"reflect"
 	"runtime"
@@ -33,6 +35,60 @@ func Exported(f interface{}) bool {
 	return ok
 }
 
+func await(v js.Value) (result js.Value, ok bool) {
+	if v.Type() != js.TypeObject || v.Get("then").Type() != js.TypeFunction {
+		return v, true
+	}
+
+	done := make(chan struct{})
+
+	onResolve := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		result = args[0]
+		ok = true
+		close(done)
+		return nil
+	})
+	defer onResolve.Release()
+
+	onReject := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		result = args[0]
+		ok = false
+		close(done)
+		return nil
+	})
+	defer onReject.Release()
+
+	v.Call("then", onResolve, onReject)
+	<-done
+	return
+}
+
+type blobReader struct {
+	index int
+
+	blob js.Value
+}
+
+func (b *blobReader) Read(bytes []byte) (int, error) {
+	if b.blob.IsUndefined() {
+		return 0, errors.New("invalid blob")
+	}
+
+	if b.index >= b.blob.Get("size").Int() {
+		return 0, io.EOF
+	}
+
+	arraybuffer, _ := await(b.blob.Call("slice", b.index, b.index+len(bytes)).Call("arrayBuffer"))
+
+	b.index += len(bytes)
+
+	i := js.CopyBytesToGo(bytes, js.Global().Get("Uint8Array").New(arraybuffer))
+
+	return i, nil
+}
+
+var _ io.Reader = new(blobReader)
+
 func Export(f interface{}) {
 	exports[runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name()] = struct{}{}
 	js.Global().Set(runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name(), js.FuncOf(func(this js.Value, args []js.Value) interface{} {
@@ -58,6 +114,14 @@ func Export(f interface{}) {
 			switch jsv.Type() {
 			case js.TypeString:
 				val = jsv.String()
+
+			case js.TypeObject:
+				if jsv.InstanceOf(js.Global().Get("Blob")) {
+					val = &blobReader{0, jsv}
+				} else {
+					panic("unsupported object type")
+				}
+
 			default:
 				val = js.Global().Get("JSON").Call("stringify", jsv).String()
 			}
@@ -65,13 +129,27 @@ func Export(f interface{}) {
 			iargs = append(iargs, val)
 		}
 
-		i, err := ctx.Call(f, iargs...)
-		if err != nil {
-			log.Println(err)
-		}
+		var resolve, reject js.Value
 
-		ctx.Return(i, err)
+		var promise = js.Global().Get("Promise").New(js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+			resolve = args[0]
+			reject = args[1]
+			return nil
+		}))
 
-		return js.Global().Get("AsyncFunction").New(buffer.String()).Invoke()
+		go func() {
+			i, err := ctx.Call(f, iargs...)
+			if err != nil {
+				log.Println(err)
+			}
+
+			ctx.Return(i, err)
+
+			v, _ := await(js.Global().Get("AsyncFunction").New(buffer.String()).Invoke())
+
+			resolve.Invoke(v)
+		}()
+
+		return promise
 	}))
 }
